3b508c80cc88ef235eea3124b32c00cfc5533978
hugo.hernandez <hugo.hernandez@sofka.com.co>
2023-03-02 11:22:11 -0500

    added: firebase config

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'password-manager-205df',
+    appId: '1:81589845259:web:f23bf50b094c931905f60e',
+    storageBucket: 'password-manager-205df.appspot.com',
+    apiKey: 'AIzaSyChVRBXvUMaIKmPT6sT2eSOo0yvWE3tmGM',
+    authDomain: 'password-manager-205df.firebaseapp.com',
+    messagingSenderId: '81589845259',
+  },
+  production: true,
+};
