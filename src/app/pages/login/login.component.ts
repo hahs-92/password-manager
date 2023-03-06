56d16e9d821d2f2a71b1576384f3c2c5947ffba8
hahs <alexandre_2420@outlook.com>
2023-03-06 17:01:43 -0500

    added: login functionality

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  @ViewChild('myForm') myForm!: NgForm;
 
+  initialForm = {
+    email: '',
+    password: '',
+  };
+
+  constructor(private authService: AuthService) {}
+
+  async onSubmit() {
+    if (this.myForm.invalid) {
+      return;
+    }
+
+    try {
+      await this.authService.login(
+        this.initialForm.email,
+        this.initialForm.password
+      );
+      console.log('Login Success');
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
